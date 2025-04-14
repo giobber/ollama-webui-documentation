f8b63438d5de46437cb9b730983644f68a569fa2
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2025-04-15 00:01:22 +0200

    Add script to generate compose files

---
+++ scripts/render_compose.py
@@ -0,0 +1,47 @@
+#!uv run
+# /// script
+# dependencies = [
+#   "jinja2",
+#   "typer",
+# ]
+# ///
+
+import os, enum
+from typing import Optional
+import typer
+import jinja2
+from pathlib import Path
+
+env = jinja2.Environment()
+cli = typer.Typer()
+
+
+class Mode(str, enum.Enum):
+    cpu = "cpu"
+    cuda = "cuda"
+    rocm = "rocm"
+
+
+def build(
+    mode: Mode = Mode.cpu,
+    output: Optional[Path] = None,
+    template_path: Path = Path("docker/_template.j2.yaml"),
+):
+    assert template_path.is_file()
+
+    if output is None:
+        output = template_path.parent / f"compose.{mode.value}.yaml"
+
+    with open(template_path, "rt") as fp:
+        template = env.from_string(fp.read())
+
+    content = template.render(mode=mode)
+
+    print(output)
+    print(content)
+    return
+    with open(output, "wt") as fp:
+        fp.write(content)
+
+
+typer.run(build)
